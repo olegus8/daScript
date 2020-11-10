48a78990edf45e384ad9a2fd8b07afacd5b5e4ce
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-10 13:09:06 -0500

    working on modules

--- utils/dasGenForEach/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from os import path
-
-if __name__ == '__main__':
-    sys.path.append(path.join(path.dirname(__file__),
-        '..', '..', 'src', 'python'))
-    from das.gen_for_each.gen_for_each import GenForEach
-    GenForEach(argv=sys.argv).run()
